6bb5462e47992b1519460cb517d0b2e392515464
jotone <hereyouare1987@gmail.com>
2017-12-26 18:02:50 +0200

    alpha 3

--- js/alpha.3.js
+++ js/alpha.3.js
@@ -1,4 +1,6 @@
-G = 2.429573 * Math.pow(10, -36);
+G = 6.67408 * Math.pow(10, -11);
+
+G2 = 2.429573 * Math.pow(10, -36);
 c = 299792458;
 screen = {
 	x: 1000,
@@ -56,28 +58,116 @@ $(document).ready(function() {
 		$('.main').append('<span class="coord-y" style="top: '+(i-14)+'px">'+(i)+'</span>');
 	}
 // /background layer
-
 	var Sol = {
-		mass: 1.9885 * Math.pow(10, 7),
+		mass:{
+			mantis: 1.9885,
+			pow: 30
+		},
 		coords: {
 			x: 500,
 			y: 500
 		},
-		radius: 25,
-		color: 'yellow'
+		radius: 695.51 * Math.pow(10, 6) / c, //Ls
+		color: 'yellow',
+		gravityDistance: ((3 * 1.9885) / (4 * 25)) * Math.pow(10, 14) /2
 	};
 
 	var planets = {
-		1: {
-			mass: 3.33022,
-			coords: {
-				x: Sol.coords.x - 100,
-				y: Sol.coords.y - 200
-			},
-			radius: 3,
-			color: 'orange',
-			moveAngle: toRad(250),
-			velocity: 140// Ls/sec
+		1: { //Mercury
+			mass:{
+				mantis: 0.333022,
+				pow: 24
+			}, //tons
+			radius: 2.4397 * Math.pow(10, 6) / c, //Ls
+			a: 0.46001009 * Math.pow(10, 11)/ c,
+			b: 0.69817445 * Math.pow(10, 11)/ c,
+			velocity: 47360 / c, //Orbital speed
+			horAngle: toRad(29.124), //pericenter Argument
+			vertAngle: 3.38 //incline per Star equator
+		},
+		2: { //Venus
+			mass: {
+				mantis:4.8675,
+				pow: 24
+			}, //tons
+			radius: 6.0518 * Math.pow(10, 6) / c, //Ls
+			a: 1.07476259 * Math.pow(10, 11)/ c,
+			b: 1.08942109 * Math.pow(10, 11)/ c,
+			velocity: 35020 / c,
+			horAngle: toRad(54.852),
+			vertAngle: 3.86
+		},
+		3: { //Earth
+			mass: {
+				mantis: 5.9726,
+				pow: 24
+			}, //tons
+			radius: 6.3781 * Math.pow(10, 6) / c, //Ls
+			a: 1.47098290 * Math.pow(10, 11)/ c,
+			b: 1.52098232 * Math.pow(10, 11)/ c,
+			velocity: 29783 / c,
+			horAngle: toRad(114.208),
+			vertAngle: 7.115
+		},
+		4: { //Mars
+			mass: {
+				mantis: 0.64171,
+				pow: 24
+			}, //tons
+			radius: 3.3962 * Math.pow(10, 6) / c, //Ls
+			a: 2.06655 * Math.pow(10, 11) / c,
+			b: 2.49232 * Math.pow(10, 11) / c,
+			velocity: 24130 / c,
+			horAngle: toRad(286.462),
+			vertAngle: 5.65
+		},
+		5: { //Upiter
+			mass: {
+				mantis: 1898.6,
+				pow: 24
+			},//tons
+			radius: 71.492 * Math.pow(10, 6) / c, //Ls
+			a: 7.405736 * Math.pow(10, 11) / c,
+			b: 8.165208 * Math.pow(10, 11) / c,
+			velocity: 13070 / c,
+			horAngle: toRad(275.066),
+			vertAngle: 6.09
+		},
+		6: { //Saturn
+			mass: {
+				mantis: 568.46,
+				pow: 24
+			}, //tons
+			radius: 60.268 * Math.pow(10, 6) / c, //Ls
+			a: 13.53572956 * Math.pow(10, 11) / c,
+			b: 15.13325783 * Math.pow(10, 11) / c,
+			velocity: 9690 / c,
+			horAngle: toRad(336.013),
+			vertAngle: 5.51
+		},
+		7: { //Uranus
+			mass: {
+				mantis: 86.832,
+				pow: 24
+			}, //tons
+			radius: 25.559 * Math.pow(10, 6) / c, //Ls
+			a: 27.48938461 * Math.pow(10, 11) / c,
+			b: 30.04419704 * Math.pow(10, 11) / c,
+			velocity: 6810 / c,
+			horAngle: toRad(96.541),
+			vertAngle: 6.48
+		},
+		8: { //Neptune
+			mass: {
+				mantis: 102.43,
+				pow: 24
+			}, //tons
+			radius: 24.764 * Math.pow(10, 6) / c, //Ls
+			a: 44.52940833 * Math.pow(10, 11) / c,
+			b: 45.53946490 * Math.pow(10, 11) / c,
+			velocity: 5434.9 / c,
+			horAngle: toRad(265.647),
+			vertAngle: 6.43
 		}
 	}
 
@@ -90,20 +180,43 @@ $(document).ready(function() {
 	ctxBg.closePath();
 // /Draw star
 
+
+	var x = planets[1].a * Math.cos(0);
+	var y = planets[1].b * Math.sin(0);
+
+	for(var i = 1; i<9; i++){
+		var x = planets[i].a * Math.cos(0);
+		var y = planets[i].b * Math.sin(0);
+
+		planets[i].coords = {
+			x: x*Math.cos(planets[i].horAngle) - y*Math.sin(planets[i].horAngle) + Sol.coords.x,
+			y: x*Math.sin(planets[i].horAngle) + y*Math.cos(planets[i].horAngle) + Sol.coords.x
+		}
+		planets[i].distance = getDistance(Sol, planets[i]);
+		planets[i].g = {
+			mod: G * (Sol.mass.mantis / planets[i].distance) * Math.pow(10, Sol.mass.pow)
+		}
+
+	}
+	console.log(planets);
+
 //Draw planet
-	ctxPl[0].beginPath();
-	ctxPl[0].arc(planets[1].coords.x, planets[1].coords.y, planets[1].radius, 0, 2* Math.PI, true);
-	ctxPl[0].fillStyle = planets[1].color;
-	ctxPl[0].fill();
-	ctxPl[0].closePath();
+	for(var i = 1; i< 4; i++){
+		ctxPl[0].beginPath();
+		ctxPl[0].arc(planets[i].coords.x, planets[i].coords.y, 1, 0, 2* Math.PI, true);
+		ctxPl[0].fillStyle = '#ee0000';
+		ctxPl[0].fill();
+		ctxPl[0].closePath();
+	}
+
 // / Draw planet
 
-	var R = getDistance(Sol, planets[1]);
+	/*var R = getDistance(Sol, planets[1]);
 	var sinE = (planets[1].coords.x - Sol.coords.x) / R;
+	var mod_g = G * (Sol.mass / R);
 	var g = {
 		x: Sol.coords.x + (planets[1].coords.x - Sol.coords.x)*(0 - sinE),
 		y: Sol.coords.y + (planets[1].coords.y - Sol.coords.y)*(0 - sinE),
-		mod: G * (Sol.mass / R)
 	};
 
 	var a = {
@@ -145,5 +258,5 @@ $(document).ready(function() {
 	ctxPl[0].lineTo(dA.x, dA.y);
 	ctxPl[0].stroke();
 	ctxPl[0].closePath();
-// / Create dA vector
+// / Create dA vector*/
 });
--- js/calculates.js
+++ js/calculates.js
@@ -7,41 +7,108 @@ c = 299792458;
 G = 2.429573 * Math.pow(10, -36); // (Ls^3) / (Ton * second^2)
 
 var Soll = {
-	mass: 1.9885 * Math.pow(10, 27), //tons,
+	mass: {
+		mantis: 1.9885,
+		pow: 27
+	}, //tons,
 	radius: 695.51 * Math.pow(10, 6) / c, //Ls
 }
 
 var planets = {
-	1: {
-		mass: 0.333022 * Math.pow(10, 21), //tons,
+	1: { //Mercury
+		mass:{
+			mantis: 0.333022,
+			pow: 21
+		}, //tons
 		radius: 2.4397 * Math.pow(10, 6) / c, //Ls
+		a: 0.46001009 * Math.pow(10, 11)/ c,
+		b: 0.69817445 * Math.pow(10, 11)/ c,
+		velocity: 47360 / c, //Orbital speed
+		horAngle: 29.124, //pericenter Argument
+		vertAngle: 3.38 //incline per Star equator
 	},
-	2: {
-		mass: 4.8675 * Math.pow(10, 21), //tons,
+	2: { //Venus
+		mass: {
+			mantis:4.8675,
+			pow: 21
+		}, //tons
 		radius: 6.0518 * Math.pow(10, 6) / c, //Ls
+		a: 1.07476259 * Math.pow(10, 11)/ c,
+		b: 1.08942109 * Math.pow(10, 11)/ c,
+		velocity: 35020 / c,
+		horAngle: 54.852,
+		vertAngle: 3.86
 	},
-	3: {
-		mass: 5.9726 * Math.pow(10, 21), //tons,
+	3: { //Earth
+		mass: {
+			mantis: 5.9726,
+			pow: 21
+		}, //tons
 		radius: 6.3781 * Math.pow(10, 6) / c, //Ls
+		a: 1.47098290 * Math.pow(10, 11)/ c,
+		b: 1.52098232 * Math.pow(10, 11)/ c,
+		velocity: 29783 / c,
+		horAngle: 114.208,
+		vertAngle: 7.115
 	},
-	4: {
-		mass: 0.64171 * Math.pow(10, 21), //tons,
+	4: { //Mars
+		mass: {
+			mantis: 0.64171,
+			pow: 21
+		}, //tons
 		radius: 3.3962 * Math.pow(10, 6) / c, //Ls
+		a: 2.06655 * Math.pow(10, 11) / c,
+		b: 2.49232 * Math.pow(10, 11) / c,
+		velocity: 24130 / c,
+		horAngle: 286.462,
+		vertAngle: 5.65
 	},
-	5: {
-		mass: 1898.6 * Math.pow(10, 21), //tons,
+	5: { //Upiter
+		mass: {
+			mantis: 1898.6,
+			pow: 21
+		},//tons
 		radius: 71.492 * Math.pow(10, 6) / c, //Ls
+		a: 7.405736 * Math.pow(10, 11) / c,
+		b: 8.165208 * Math.pow(10, 11) / c,
+		velocity: 13070 / c,
+		horAngle: 275.066,
+		vertAngle: 6.09
 	},
-	6: {
-		mass: 568.46 * Math.pow(10, 21), //tons,
+	6: { //Saturn
+		mass: {
+			mantis: 568.46,
+			pow: 21
+		}, //tons
 		radius: 60.268 * Math.pow(10, 6) / c, //Ls
+		a: 13.53572956 * Math.pow(10, 11) / c,
+		b: 15.13325783 * Math.pow(10, 11) / c,
+		velocity: 9690 / c,
+		horAngle: 336.013,
+		vertAngle: 5.51
 	},
-	7: {
-		mass: 86.832 * Math.pow(10, 21), //tons,
+	7: { //Uranus
+		mass: {
+			mantis: 86.832,
+			pow: 21
+		}, //tons
 		radius: 25.559 * Math.pow(10, 6) / c, //Ls
+		a: 27.48938461 * Math.pow(10, 11) / c,
+		b: 30.04419704 * Math.pow(10, 11) / c,
+		velocity: 6810 / c,
+		horAngle: 96.541,
+		vertAngle: 6.48
 	},
-	8: {
-		mass: 102.43 * Math.pow(10, 21), //tons,
+	8: { //Neptune
+		mass: {
+			mantis: 102.43,
+			pow: 21
+		}, //tons
 		radius: 24.764 * Math.pow(10, 6) / c, //Ls
+		a: 44.52940833 * Math.pow(10, 11) / c,
+		b: 45.53946490 * Math.pow(10, 11) / c,
+		velocity: 5434.9 / c,
+		horAngle: 265.647,
+		vertAngle: 6.43
 	}
 }
